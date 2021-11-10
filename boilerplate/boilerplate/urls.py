8eb4b1712db52822f790170964d5d202c898b53a
Louis Andrew <louisandrew3@gmail.com>
2021-11-10 22:57:25 +0100

    feat(/product/:product_id): setup endpoint to retrieve a single product

--- boilerplate/boilerplate/urls.py
+++ boilerplate/boilerplate/urls.py
@@ -5,9 +5,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from os import environ
 
-# ! Cannot be found. @see https://stackoverflow.com/questions/47563013/unable-to-import-path-from-django-urls
-# from django.urls import path
-
 app_name = environ.get('DJANGO_APP_NAME', default="local")
 
 protocol = 'http' if environ.get('DEBUG', default=1) == 1 else 'https'
