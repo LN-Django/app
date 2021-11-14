c86047d9a5b9f5836805dde418a3185bdf4a0499
Louis Andrew <louisandrew3@gmail.com>
2021-11-14 22:33:12 +0100

    feat(urls): add name to service endpoints

--- boilerplate/main_service/urls.py
+++ boilerplate/main_service/urls.py
@@ -7,6 +7,6 @@ from .views.product import ProductView
 
 urlpatterns = [
     url('api/ping_main', SampleView.as_view()),
-    url('api/products', ProductsListView.as_view()),
-    path('api/product/<int:product_id>', ProductView.as_view())
+    url('api/products', ProductsListView.as_view(), name='get_post_products'),
+    path('api/product/<int:product_id>', ProductView.as_view(), name='get_single_product')
 ]
