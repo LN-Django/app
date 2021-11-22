2c6990f3e9d9caf52cfd701769dfa819c0f65b8f
Louis Andrew <louisandrew3@gmail.com>
2021-11-22 17:04:13 +0100

    feat(ping_main): add simple logger to controller

--- boilerplate/main_service/views/sample.py
+++ boilerplate/main_service/views/sample.py
@@ -1,11 +1,14 @@
+import logging
 from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
 
 
 class SampleView(APIView):
+    logger = logging.getLogger('mainLogger')
     permission_classes = [permissions.AllowAny]
 
     def get(self, request, format=None):
+        self.logger.info('Test log')
         data = {'foo': 'bar!'}
         return Response(data)
